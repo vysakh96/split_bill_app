304631ca20f2a287397cabff7f9aed916572f39e
vysakh96 <118036322+vysakh96@users.noreply.github.com>
2022-12-10 16:19:00 +0530

    completed

---
+++ lib/main.dart
@@ -0,0 +1,404 @@
+
+import 'package:flutter/material.dart';
+import 'package:split_bill/page_2.dart';
+
+void main() {
+  runApp(MaterialApp(debugShowCheckedModeBanner: false,
+  home: const SplitBill(),),
+  );
+}
+class SplitBill extends StatefulWidget {
+  const SplitBill({Key? key}) : super(key: key);
+
+  @override
+  State<SplitBill> createState() => _SplitBillState();
+}
+
+class _SplitBillState extends State<SplitBill> {
+  double slide = 0;
+  int count = 0;
+  TextEditingController taxController = TextEditingController();
+
+  void increment(){
+    setState(() {
+      count++;
+    });
+  }
+  void decrement(){
+    if(count==0){
+      count =0;
+    }else{
+      setState(() {
+        count--;
+      });
+
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('SPLIT BILL',
+          style: TextStyle(fontWeight: FontWeight.bold,color: Colors.black),),
+        backgroundColor: Colors.white,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Container(
+                height: 150,
+                width: double.maxFinite,
+                color: Colors.amberAccent,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Total',
+                            style: TextStyle(fontSize:30,fontWeight: FontWeight.bold ),
+                          ),
+                          Text('$number',
+                            style: TextStyle(fontSize:30,fontWeight: FontWeight.bold ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                          Text('Friends',
+                            style: TextStyle(fontSize:30,fontWeight: FontWeight.bold ),
+                          ),
+                          Text('Tax',
+                            style: TextStyle(fontSize:30,fontWeight: FontWeight.bold ),
+                          ),
+                          Text('Tip',
+                            style: TextStyle(fontSize:30,fontWeight: FontWeight.bold ),
+                          ),
+                            ],
+                          ),
+                      SizedBox(width: 10,),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('${slide.round().toString()}',
+                            style: TextStyle(fontSize:30,fontWeight: FontWeight.bold ),
+                          ),
+                          Text('$tax %',
+                            style: TextStyle(fontSize:30,fontWeight: FontWeight.bold ),
+                          ),
+                          Text('$count',
+                            style: TextStyle(fontSize:30,fontWeight: FontWeight.bold ),
+                          ),
+                        ],
+                      ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Text('How Many Friends ?',
+              style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),
+            ),
+            Slider(
+                value: slide,
+                activeColor: Colors.amber,
+                inactiveColor: Colors.black,
+                max: 100,
+                divisions: 100,
+                onChanged: (value){
+                  setState(() {
+                    slide = value;
+                  });
+                }),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(flex: 2,
+                    child: Container(
+                      height: 90,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                        color: Colors.amberAccent,),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            FloatingActionButton(onPressed: (){
+                              decrement();
+                            },
+                              backgroundColor: Colors.grey,
+                              child:Text('-',style: TextStyle(fontSize: 30),) ,
+                            ),
+                            Column(
+                              children: [
+                                Text('TIP',
+                                  style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),),
+                                Text('$count',
+                                  style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),),
+                              ],
+                            ),
+                            FloatingActionButton(onPressed: (){
+                              increment();
+                            },
+                              backgroundColor: Colors.grey,
+                              child:Icon(Icons.add) ,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 20,),
+                  Expanded(
+                    child: Container(
+                      height: 90,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.amberAccent,),
+
+                      child: Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: Align(alignment: Alignment.center,
+                          child: TextField(controller: taxController,
+                            onChanged: (value){
+                            getTax(value);
+
+                          },
+                            decoration: InputDecoration(
+                              label: Text('Tax in %'),
+                              border: OutlineInputBorder(borderRadius: BorderRadius.circular(20))
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 5,),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: OutlinedButton(onPressed: (){
+                      getFriendsValue('1');
+                    },
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 20),
+                          child: Text('1',
+                            style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20,color: Colors.black,),),
+                        ),
+                    ),
+                  ),
+                  Expanded(
+                    child: OutlinedButton(onPressed: (){
+                      getFriendsValue('2');
+                    },
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 20),
+                        child: Text('2',
+                          style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20,color: Colors.black),),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: OutlinedButton(onPressed: (){
+                      getFriendsValue('3');
+                    },
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 20),
+                        child: Text('3',
+                          style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20,color: Colors.black),),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: OutlinedButton(onPressed: (){
+                      getFriendsValue('4');
+                    },
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 20),
+                        child: Text('4',
+                          style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20,color: Colors.black,),),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: OutlinedButton(onPressed: (){
+                      getFriendsValue('5');
+                    },
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 20),
+                        child: Text('5',
+                          style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20,color: Colors.black),),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: OutlinedButton(onPressed: (){
+                      getFriendsValue('6');
+                    },
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 20),
+                        child: Text('6',
+                          style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20,color: Colors.black),),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: OutlinedButton(onPressed: (){
+                      getFriendsValue('7');
+                    },
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 20),
+                        child: Text('7',
+                          style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20,color: Colors.black,),),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: OutlinedButton(onPressed: (){
+                      getFriendsValue('8');
+                    },
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 20),
+                        child: Text('8',
+                          style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20,color: Colors.black),),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: OutlinedButton(onPressed: (){
+                      getFriendsValue('9');
+                    },
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 20),
+                        child: Text('9',
+                          style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20,color: Colors.black),),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: OutlinedButton(onPressed: (){
+                      getFriendsValue('.');
+                    },
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 20),
+                        child: Text('.',
+                          style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20,color: Colors.black,),),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: OutlinedButton(onPressed: (){
+                      getFriendsValue('0');
+                    },
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 20),
+                        child: Text('0',
+                          style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20,color: Colors.black),),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: OutlinedButton(onPressed: (){
+                      clear();
+                    },
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 20),
+                        child: Text('CLR',
+                          style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20,color: Colors.black),),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10.0),
+              child: Container(
+                height: 50,
+                width: double.maxFinite,
+                color: Colors.green[900],
+                child:TextButton(onPressed: (){
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context)=>ResultPage(slide,count,tax,number)));
+                },
+                  child: Text('Split Bill',
+                    style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,color: Colors.white),
+                  ),
+                ) ,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+  int tax = 0;
+  void getTax(String value) {
+    setState(() {
+      tax = int.parse(value);
+    });
+
+  }
+  String number='';
+
+  void getFriendsValue(String s) {
+    setState(() {
+      number += s;
+    });
+  }
+
+  void clear() {
+    setState(() {
+      number = '';
+      tax = 0;
+      count = 0;
+      slide = 0;
+      taxController.text = '';
+    });
+  }
+
+}
+
+
+
